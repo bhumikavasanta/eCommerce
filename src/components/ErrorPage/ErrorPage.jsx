74ca8bf56a83a85c8fc536cee1544dbb4198725f
Bhumika Vasanta <bhumika.vasanta@quantiphi.com>
2023-12-26 14:24:23 +0530

    Error page added

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import './styles.css';
+import { NavLink } from "react-router-dom";
 
 const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
+    <section>
+      <div className="container">
+        <div>
+          <h2>404</h2>
+          <h3>UH OH! You're lost.</h3>
+          <p>
+            The page you are looking for does not exist. Click the button below to go back to the
+            homepage.
+          </p>
+
+          <NavLink to="/">
+            <button>Go Back to Home</button>
+          </NavLink>
+        </div>
+      </div>
+    </section>
   )
 }
 
